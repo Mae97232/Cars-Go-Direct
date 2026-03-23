bd2adc7d1b62cf45c7a8a14ce72d6832dca4f107
Mae97232 <maelyck97232@gmail.com>
2026-03-23 21:10:38 +0100

    Delete src directory

--- src/app/annonces/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getPublishedListings } from "@/lib/server/listings-search";
-import AnnoncesClient from "./annoncesclient";
-
-export default async function AnnoncesPage() {
-  const listings = await getPublishedListings();
-
-  return <AnnoncesClient listings={listings} />;
-}
--- src/app/recherche/page.tsx
+++
@@ -1,8 +0,0 @@
-import AnnoncesClient from "@/app/annonces/annoncesclient";
-import { getPublishedListings } from "@/lib/server/listings-search";
-
-export default async function RecherchePage() {
-  const listings = await getPublishedListings();
-
-  return <AnnoncesClient listings={listings} />;
-}
